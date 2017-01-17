e7181c6bde08f42704af48f9ec2fbe236c5fd9d5
akshay <ar15@iitbbs.ac.in>
2017-01-17 09:57:23 +0530

    our main class

---
+++ pathfinder.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class pathfinder{
+	public static void public static void main(String[] args) {
+		int n;//no of node
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the no. of vertex");
+		n = sc.nextInt();
+		int v[][] = new int[n][n];
+		System.out.println("Enter the Adjancency matrix");
+		for(int i = 0;i<n;i++){
+			for(int j = 0;j<n;j++)
+				v[i][j] = sc.nextInt();
+		}
+	}
+}
+class node{
+
+}
